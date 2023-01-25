e390b72edf0d748dfd6c8f873348b4e071708027
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-01-25 15:50:39 +0300

    Возврат функцией скрытого номера карты символами **...

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const truncate = (string, count) => {
-  return `${string.slice(0, count)}...`;
+const getHiddenCard = (cardAccount, stars = 4) => `${'*'.repeat(stars)}${cardAccount.slice(-4)}`;
+
+const getHiddenCard_2 = (cardAccount, stars = 4) => {
+  return `${'*'.repeat(stars)}${cardAccount.slice(-4)}`;
 };
 
-console.log(truncate('и пошла вода', 5));
+console.log(getHiddenCard_2('123456789123456', 2));
