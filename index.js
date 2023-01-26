bd763a62265067144c6ede6d9850b9678b8ef417
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-01-26 17:15:21 +0300

    Функции к разделу №36

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-const mySubstr = (string, length) => {
-  let result = '';
+//Функция находит сколько раз знак содержится в строке
+const countChars_base = (string, char) => {
+  let result = "";
+  let i = 0;
+  while (i < string.length) {
+    if (string[i] === char) {
+      result = result + char;
+    }
+    i = i + 1;
+  }
+  return result.length;
+};
+
+//console.log(countChars_base("Fear cuts deeper than swords.", "f"));
+
+//Функция находит сколько раз знак содержится в строке без привязки к регистру
+
+const countChars = (string, char) => {
+  let result = 0;
   let i = 0;
 
-  while(i < length) {
-    result = `${result}${string[i]}`;
-    i += 1;
+  while (i < string.length) {
+    if (string[i].toLowerCase() === char.toLowerCase()) {
+      result = result + 1;
+    }
+    i = i + 1;
   }
   return result;
 };
 
-const text = 'If I look back I am lost';
-console.log(mySubstr(text, 1));
-console.log(mySubstr(text, 7));
+console.log(countChars('axe', 'a'));
+console.log(countChars('', 'a'));
+console.log(countChars('OpPa', 'p'));
+console.log(countChars('OpPa', 'P'));
