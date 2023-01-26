7a4173cdf8e13cd89b566cd16f1fe7315253907d
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-01-26 11:59:48 +0300

    Пример использования конструкции Switch

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
-// Функция для проверки имени сайта на наличие https://
-const normalizeUrl = (site) => (site.startsWith('https://') ? site : `https://${site}`);
-console.log(normalizeUrl("google.com"));
-console.log(normalizeUrl("https://ai.fi"));
-
-
-// Функция для определения модуля числа
-const abs = (num) => (num >= 0 ? num : -num);
-console.log(abs(10));
-console.log(abs(-10));
-
-
-// Функция реверса строки
-const reverse = (s) => s.split("").reverse().join("");
-
-
-// Тернарный однострочник, определяющий заглавность первого знака в строке. Возвращает пустую строку, строку с первой заглавной буквой или перевернутый текст
-const convertText = (string) => (string === "" || string[0] === string[0].toUpperCase()) ? string : reverse(string);
-console.log(convertText("Hello"));
-console.log(convertText("hello"));
-console.log(convertText(""));
-
-
-// If-else-функция, определяющая заглавность первого знака в строке. Возвращает пустую строку, строку с первой заглавной буквой или перевернутый текст
-const convertTextClassic = (string) => {
-  if (string === "" || string[0] === string[0].toUpperCase()) {
-    return string;
-  } else {
-    return reverse(string);
+const getNumberExplanation = (num) => {
+  switch(num) {
+    case 666:
+      return 'devil number';
+    case 42:
+      return 'answer for everything';
+    case 7:
+      return 'prime number';
+    default: 
+      return null;
   }
 };
-console.log(convertTextClassic("Hello"));
-console.log(convertTextClassic("hello"));
-console.log(convertTextClassic(""));
+
+console.log(getNumberExplanation(8));
+console.log(getNumberExplanation(666));
+console.log(getNumberExplanation(42));
+console.log(getNumberExplanation(7));
