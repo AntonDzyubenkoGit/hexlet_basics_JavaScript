56025d6bb139cb5d2da4e83f533bc6ed3a49ed8b
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-01-26 11:45:01 +0300

    Несколько функций из упражнеий №30-31 в курсе Основы JavaScript

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-const isLeapYear = (year) => year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0);
+// Функция для проверки имени сайта на наличие https://
+const normalizeUrl = (site) => (site.startsWith('https://') ? site : `https://${site}`);
+console.log(normalizeUrl("google.com"));
+console.log(normalizeUrl("https://ai.fi"));
 
 
-console.log(isLeapYear(2018));
-console.log(isLeapYear(2017));
-console.log(isLeapYear(2016));
+// Функция для определения модуля числа
+const abs = (num) => (num >= 0 ? num : -num);
+console.log(abs(10));
+console.log(abs(-10));
+
+
+// Функция реверса строки
+const reverse = (s) => s.split("").reverse().join("");
+
+
+// Тернарный однострочник, определяющий заглавность первого знака в строке. Возвращает пустую строку, строку с первой заглавной буквой или перевернутый текст
+const convertText = (string) => (string === "" || string[0] === string[0].toUpperCase()) ? string : reverse(string);
+console.log(convertText("Hello"));
+console.log(convertText("hello"));
+console.log(convertText(""));
+
+
+// If-else-функция, определяющая заглавность первого знака в строке. Возвращает пустую строку, строку с первой заглавной буквой или перевернутый текст
+const convertTextClassic = (string) => {
+  if (string === "" || string[0] === string[0].toUpperCase()) {
+    return string;
+  } else {
+    return reverse(string);
+  }
+};
+console.log(convertTextClassic("Hello"));
+console.log(convertTextClassic("hello"));
+console.log(convertTextClassic(""));
