d0a9727f47fc92c3482acfe693c9a373613bcd98
AntonDzyubenkoGit <107909682+AntonDzyubenkoGit@users.noreply.github.com>
2023-01-27 13:55:35 +0300

    Испытания. Javascript: Фасад

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-// Javascript: Счастливый билет
-const isHappyTicket = (str) => {
-  let resultFirstHAlf = 0;
-  let resultSecondHalf = 0;
-  for (let i = 0; i < str.length / 2; i++) {
-    resultFirstHAlf += Number(str[i]);
-  }
-  for (let j = str.length / 2; j < str.length; j++) {
-    resultSecondHalf += Number(str[j]);
-  }
-  return resultFirstHAlf === resultSecondHalf;
-};
+// Испытания. Javascript: Фасад
 
-console.log(isHappyTicket("23"));
+import cube from './numbers1.js';
+
+export default cube;
+export {
+  default as multi,
+  pow2 as pow,
+  sum2 as sum,
+  sub2 as sub,
+} from './numbers2.js';
+export { default as sqrt } from './numbers3.js';
